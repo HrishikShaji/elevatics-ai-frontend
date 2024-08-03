99b8cf7a7e582b4fb53df870b22f7759f4f4cdd8
HrishikShaji <hrishikminishaji@gmail.com>
2024-08-03 19:06:06 +0530

    new typing effect added

--- app/(defaults)/search/components/TypedMarkdown.tsx
+++ app/(defaults)/search/components/TypedMarkdown.tsx
@@ -1,25 +1,48 @@
 "use client";
 
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect, useRef } from 'react';
 import ReactMarkdown from "react-markdown";
 import rehypeRaw from 'rehype-raw';
 import remarkGfm from 'remark-gfm';
 import styles from "../../../../styles/cursor.module.css";
 import { marked } from 'marked';
 
+const useTyping = (text: string, delay = 10) => {
+    const [currentText, setCurrentText] = useState('');
+    const [currentIndex, setCurrentIndex] = useState(0);
+    useEffect(() => {
+        if (currentIndex < text.length) {
+            const timeout = setTimeout(() => {
+                setCurrentText(prevText => prevText + text[currentIndex]);
+                setCurrentIndex(prevIndex => prevIndex + 1);
+            }, delay);
+
+            return () => clearTimeout(timeout);
+        }
+    }, [currentIndex, delay, text]);
+
+    return currentText
+}
+
+
 const useTypewriter = (text: string, speed = 50) => {
     const [displayedText, setDisplayedText] = useState('');
     const [position, setPosition] = useState(0);
+    const intervalRef = useRef<NodeJS.Timeout | null>(null);
 
     useEffect(() => {
         if (position >= text.length) return;
 
-        const intervalId = setInterval(() => {
+        intervalRef.current = setInterval(() => {
             setDisplayedText((prev) => prev + text[position]);
             setPosition((prev) => prev + 1);
         }, speed);
 
-        return () => clearInterval(intervalId);
+        return () => {
+            if (intervalRef.current) {
+                clearInterval(intervalRef.current);
+            }
+        };
     }, [text, position, speed]);
 
     return displayedText;
@@ -30,9 +53,10 @@ interface TypedMarkdownProps {
 }
 
 export default function TypedMarkdown({ text }: TypedMarkdownProps) {
-    const displayedText = useTypewriter(text, 10);
+    const displayedText = useTypewriter(text, 1);
+    const newContent = useTyping(text, 1)
 
-    const markdownWithCursor = `${displayedText} <span class="cursor"></span>`;
+    const markdownWithCursor = `${newContent} <span class="cursor"></span>`;
 
     return (
         <ReactMarkdown
