761180616b95d608297ee9e316444ecc35844257
HrishikShaji <hrishikminishaji@gmail.com>
2024-08-15 09:38:19 +0530

    removed doument

--- app/(defaults)/document/page.tsx
+++
@@ -1,9 +0,0 @@
-import { ChatProvider } from "@/contexts/ChatContext";
-import ChatWindow from "@/components/chat/ChatWindow";
-
-export default function Page() {
-    return <ChatProvider>
-        <ChatWindow title="Document" subTitle='Efficient Document Search' responseType='document' initialChatHistory='' reportId='' disable={false} />
-    </ChatProvider>
-
-}
