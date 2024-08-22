6c2200362d80cad86b23fe942be4ac06a67c2258
HrishikShaji <hrishikminishaji@gmail.com>
2024-08-22 09:35:57 +0530

    added suggestions

--- app/(defaults)/agents/components/QuickChat.tsx
+++ app/(defaults)/agents/components/QuickChat.tsx
@@ -5,12 +5,12 @@
 
 import ChatWindow from "@/components/chat/ChatWindow"
 import { ChatProvider } from "@/contexts/ChatContext"
-import { agents } from "@/lib/constants"
+import { agents, suggestions } from "@/lib/constants"
 
 export default function QuickChat() {
     const agent = agents["QUICK"]
     return <ChatProvider>
-        <ChatWindow title={agent.name} subTitle={agent.tagLine} responseType="iresearcher-report" initialChatHistory='' reportId='' disable={false} />
+        <ChatWindow title={agent.name} subTitle={agent.tagLine} responseType="iresearcher-report" suggestions={suggestions} initialChatHistory='' reportId='' disable={false} />
     </ChatProvider>
 
 }
