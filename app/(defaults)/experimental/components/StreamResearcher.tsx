9660e0856ae6a4499c1506aee4a1ff06e3d04cc6
HrishikShaji <hrishikminishaji@gmail.com>
2024-08-23 18:26:16 +0530

    new endpoint added

--- app/(defaults)/experimental/components/StreamResearcher.tsx
+++ app/(defaults)/experimental/components/StreamResearcher.tsx
@@ -8,6 +8,7 @@ import style from "@/styles/medium.module.css"
 import remarkGfm from "remark-gfm"
 import rehypeRaw from "rehype-raw"
 import dynamic from "next/dynamic";
+import { LATEST_RESEARCHER_REPORT_URL, NEW_RESEARCHER_REPORT_URL } from "@/lib/endpoints";
 
 type Topic = {
     isCompleted: boolean;
@@ -29,7 +30,7 @@ function delay(ms) {
     return new Promise(resolve => setTimeout(resolve, ms));
 }
 async function fetchReport({ topic, addReport, addReports }: { topic: Topic, addReport: (report: string) => void, addReports: (report: Report) => void }) {
-    const response = await fetch('https://pvanand-search-generate-prod.hf.space/generate_report', {
+    const response = await fetch(NEW_RESEARCHER_REPORT_URL, {
         method: 'POST',
         headers: {
             'Content-Type': 'application/json',
@@ -64,22 +65,22 @@ async function fetchReport({ topic, addReport, addReports }: { topic: Topic, add
 
             const chunk = decoder.decode(value, { stream: true });
 
-            if (chunk.includes('<report-metadata>')) {
+            if (chunk.includes('<json>')) {
                 isReadingMetadata = true;
                 metadata = '';
             }
 
             if (isReadingMetadata) {
                 metadata += chunk;
-                if (chunk.includes('</report-metadata>')) {
+                if (chunk.includes('</json>')) {
                     isReadingMetadata = false;
                 }
             } else {
-                for (let i = 0; i < chunk.length; i += 100) {
-                    const batch = chunk.slice(i, i + 100);
+                for (let i = 0; i < chunk.length; i += 10) {
+                    const batch = chunk.slice(i, i + 10);
                     markdown += batch;
                     addReport(markdown);
-                    await delay(100);
+                    await delay(1);
                 }
             }
         }
@@ -120,7 +121,6 @@ export default function StreamResearcher() {
     }
 
 
-    const debouncedAddReport = useCallback(debounce(addReport, 100), []);
     function addReports(report: Report) {
         setReports(prev => [...prev, report]);
     }
@@ -132,7 +132,7 @@ export default function StreamResearcher() {
                 setStreaming(true)
                 setStreamingReport("")
                 setStreamingReportParent(topic.parentKey)
-                await fetchReport({ topic: topic, addReport: debouncedAddReport, addReports });
+                await fetchReport({ topic: topic, addReport: addReport, addReports });
             } catch (error) {
                 console.log(error)
             } finally {
@@ -142,30 +142,36 @@ export default function StreamResearcher() {
     }
 
     return (
-        <div className="flex flex-col gap-10 p-10">
-            <button className="p-2 bg-black text-white" onClick={generateReports} disabled={streaming}>
-                Generate
-            </button>
-            <div className="flex gap-2 p-1 rounded-md bg-neutral-900 ">
-                {sliderData.map((parent, i) => (
-                    <button onClick={() => setCurrentParent(parent)} key={i} className="py-1 px-2 rounded-md bg-black text-white">{parent}</button>
-                ))}
+        <div className="flex flex-col gap-5 py-10">
+            <div className="flex justify-center">
+                <button className="px-2 py-1 rounded-md bg-neutral-700 text-white" onClick={generateReports} disabled={streaming}>
+                    Generate
+                </button>
             </div>
-            <div className="flex scroll-smooth  flex-col-reverse h-[70vh] overflow-y-auto items-center w-full custom-scrollbar">
-                <div className="flex flex-col gap-10">
-                    {reports.map((report, i) => (
-                        <div style={{ display: currentParent === report.parentKey ? "block" : "none" }} className="w-[1000px] p-5 rounded-3xl bg-gray-100" key={i}>
-                            <div className={style.markdown}>
-                                <MemoizedMarkdown content={report.content} />
+            <div className="flex flex-col gap-2">
+                <div className="flex justify-center">
+                    <div className="flex w-[1000px] justify-center gap-2 p-1 rounded-md bg-neutral-900 overflow-x-hidden">
+                        {sliderData.map((parent, i) => (
+                            <button onClick={() => setCurrentParent(parent)} key={i} className="py-1 px-2 min-w-[300px] rounded-md bg-neutral-700 text-white">{parent}</button>
+                        ))}
+                    </div>
+                </div>
+                <div className="flex scroll-smooth  flex-col-reverse h-[70vh] overflow-y-auto items-center w-full custom-scrollbar">
+                    <div className="flex flex-col gap-10">
+                        {reports.map((report, i) => (
+                            <div style={{ display: currentParent === report.parentKey ? "block" : "none" }} className="w-[1000px] p-5 rounded-3xl bg-gray-100" key={i}>
+                                <div className={style.markdown}>
+                                    <MemoizedMarkdown content={report.content} />
+                                </div>
                             </div>
-                        </div>
-                    ))}
-                    {streaming ?
-                        <div style={{ display: currentParent === streamingReportParent ? "block" : "none" }} className="w-[1000px] p-5 rounded-3xl ">
-                            <div className={style.markdown}>
-                                <MemoizedMarkdown content={streamingReport} />
-                            </div>
-                        </div> : null}
+                        ))}
+                        {streaming ?
+                            <div style={{ display: currentParent === streamingReportParent ? "block" : "none" }} className="w-[1000px] p-5 rounded-3xl ">
+                                <div className={style.markdown}>
+                                    <MemoizedMarkdown content={streamingReport} />
+                                </div>
+                            </div> : null}
+                    </div>
                 </div>
             </div>
         </div>
@@ -181,6 +187,15 @@ const components: Components = {
     },
     "report-metadata": ({ node, ...props }: ExtraProps) => {
         return null;
+    },
+    "report": ({ node, ...props }: ExtraProps) => {
+        return <div className="bg-yellow-500" {...props} />
+    },
+    "report-chart": ({ node, ...props }: ExtraProps) => {
+        return <div className="bg-green-500" {...props} />
+    },
+    table: ({ node, ...props }) => {
+        return <div className="bg-red-500">got table</div>
     },
     script: memo(({ node, src, children }) => {
         if (src === "https://cdn.plot.ly/plotly-latest.min.js") {
